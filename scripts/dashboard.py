d68223e0e19d40fb4698c99645c6b847bf80919a
Ryan Richards <ryan@m4.local>
2024-11-13 18:06:47 -0800

    class integration and nivo pie chart

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+from streamlit_elements import elements, mui, html, nivo
+
 
 # player information (current globals)
 df = pd.read_csv(f'../data/players.csv')
 players = df.name.values.tolist()
 df_teams = df.groupby('team').sum()['total_points'].reset_index().sort_values('total_points', ascending=False)
+pie_data=[]
+for i in range(12):
+    pie_data.append({"id": df.iloc[i]['name'], "label": df.iloc[i]['name'], "value": df.iloc[i]['now_cost']/10.})
 ############################################
 
 st.set_page_config(
@@ -63,6 +68,9 @@ if __name__ == '__main__':
     dash.create_field(col_num=0)
     dash.create_topteams(col_num=1)
 
+
+    timeline = st.sidebar.slider("Timeline", 0.0, 1.0, 0.5)
+
     # Sidebar for player selection
     with st.sidebar.expander("Team Selection", expanded=False):
         selected_player = st.sidebar.selectbox("Select a player", players)
@@ -77,3 +85,24 @@ if __name__ == '__main__':
         ax.plot(x, y)
         ax.set(xlabel='X-axis', ylabel='Y-axis', title='REPLACE')
         st.pyplot(fig)
+
+
+    with st.sidebar.expander('Budget', expanded=True):
+
+        with elements("nivo_pie_chart"):
+            with mui.Box(sx={"height": 500}):
+                nivo.Pie(
+                    data=pie_data,
+                    margin={"top": 40, "right": 80, "bottom": 80, "left": 80},
+                    innerRadius=0.5,
+                    padAngle=0.7,
+                    cornerRadius=3,
+                    colors={ "scheme": "nivo" },
+                    borderWidth=1,
+                    borderColor={"from": "color", "modifiers": [["darker", 0.2]]},
+                    radialLabelsSkipAngle=10,
+                    radialLabelsTextColor="#333333",
+                    radialLabelsLinkColor={"from": "color"},
+                    sliceLabelsSkipAngle=10,
+                    sliceLabelsTextColor="#333333"
+                )
