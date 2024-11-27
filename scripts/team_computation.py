3dc0aca7a4c6796fa821ac3a818b6bcd4f398b73
Arnav Dixit <arnavdixit@Arnavs-MacBook-Air.local>
2024-11-27 15:12:05 -0800

    Added player similarity function in team_computations.py

--- scripts/team_computation.py
+++ scripts/team_computation.py
@@ -2,6 +2,9 @@
 
 import pandas as pd
 from constants import FORMATION_MAP
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial.distance import cdist
+import umap
 
 def get_top_players_by_position(player_data, formation):
     """
@@ -87,3 +90,41 @@ def adjust_team_to_budget(team, budget, player_data):
             continue  # No cheaper players available for this position
 
     return adjusted_team
+
+def get_similar_players(df: pd.DataFrame, player_name:str, top_n: int = 5):
+    
+    """Returns top_n similar players based on the player input. Uses UMAP for dimensional reduction and euclidean distance to find similarities
+       Requires sklearn, scipy and umap modules.   
+    """
+    
+    df["now_cost_m"] = df["now_cost"]/10
+    target_position = df.loc[df['web_name'] == player_name, 'position'].values[0]
+    if target_position == 'GKP':
+        numeric_features = ["now_cost_m", "total_points", "minutes", "goals_conceded", "clean_sheets", "ict_index"]
+    elif target_position == 'DEF':
+        numeric_features = ["now_cost_m", "total_points", "minutes", "goals_conceded", "clean_sheets", "assists", "creativity", "goals_scored", "ict_index"]
+    elif target_position == "MID":
+        numeric_features = ["now_cost_m", "total_points", "minutes", "goals_scored", "assists", "creativity", "influence", "threat", "goals_conceded", "clean_sheets", "ict_index"]
+    else:
+        numeric_features = ["now_cost_m", "total_points", "minutes", "goals_scored", "assists", "creativity", "influence", "threat", "ict_index"]
+    metadata = ['web_name', 'position']
+    filtered_data = df[metadata + numeric_features].dropna()
+
+    
+    scaler = StandardScaler()
+    normalized_features = scaler.fit_transform(filtered_data[numeric_features])
+
+    umap_reducer = umap.UMAP(n_neighbors=5, min_dist=0.1, n_components=2)
+    umap_features = umap_reducer.fit_transform(normalized_features)
+
+    distance_matrix_umap = cdist(umap_features, umap_features, metric='euclidean')
+
+    distance_df_umap = pd.DataFrame(distance_matrix_umap, index=filtered_data['web_name'], columns=filtered_data['web_name'])
+
+    if player_name not in distance_df_umap.index:
+        return f"Player '{player_name}' not found in the dataset."
+
+    same_position_players = df[df['position'] == target_position]['web_name']
+    distances = distance_df_umap.loc[player_name, same_position_players]
+    similar_players = distances.sort_values()[1:top_n+1]  # Exclude self (distance = 0)
+    return similar_players
